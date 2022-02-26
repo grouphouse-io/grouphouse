aca7fd8f047c7baded6249896d5a99cb44f73b12
Jack <jackrosenberg99@gmail.com>
2022-02-25 20:00:13 -0800

    Resized all images down for performance

--- services/web-frontend/internal/src/ui/containers/Storyline.tsx
+++ services/web-frontend/internal/src/ui/containers/Storyline.tsx
@@ -23,7 +23,7 @@ export class Storyline extends React.Component<PropsType> {
           <div className="storyline-section-hook">
             American community is disappearing.
           </div>
-            <img src="/images/grouphouse-entrance-nighttime.jpg" className="storyline-image-left" alt="Gloom"></img>
+            <img src="/images/grouphouse-entrance-nighttime-min.jpg" className="storyline-image-left" alt="Gloom"></img>
             <div className="storyline-section-text-right">
               There is a shared but divided struggle of feeling unsupported, unsafe, alone.
               Lack of community has raised the stakes in romantic relationships, marriage, and family, placing extraordinary and unhealthy levels of stress on these relations.
@@ -38,7 +38,7 @@ export class Storyline extends React.Component<PropsType> {
           <div className="storyline-section-hook">
             Rebuild safety by rebuilding community.
           </div>
-          <img src="/images/grouphouse-flower.jpg" className="storyline-image-right"alt="Hope"></img>
+          <img src="/images/grouphouse-flower-min.jpg" className="storyline-image-right"alt="Hope"></img>
           <div className="storyline-section-text-left">
             Home means safety. Our mission is to rebuild community through intentional group-living, so that you can find your people and your home.
           </div>
@@ -52,7 +52,7 @@ export class Storyline extends React.Component<PropsType> {
           <div className="storyline-section-hook">
             We build tools for better living.
           </div>
-          <img src="/images/grouphouse-tea2.jpg" className="storyline-image-left" alt="Tea"></img>
+          <img src="/images/grouphouse-porch-daytime-min.jpg" className="storyline-image-left" alt="Porch"></img>
           <div className="storyline-section-text-right">
             Group-living requires skin in the game.
             It's hard. But you get out what you put in. We crowdsource tools to make it easier.
